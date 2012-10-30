e8f4c5f70e998f2d4a2e885efce787c738c8bd28
Daniel <gaeta.d@gmail.com>
2012-10-29 23:31:58 -0500

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,8 @@ skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.f
     @user = User.new
 
    respond_to do |format|
-      #format.html # new.html.erb
-      #format.json { render json: @user }
+      format.html # new.html.erb
+      format.json { render json: @user }
    end
 
   end
