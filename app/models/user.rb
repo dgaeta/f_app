a0521bfda66f7b33a94b1b0bf0f621cd2f46108b
Daniel <gaeta.d@gmail.com>
2012-11-09 01:15:17 -0600

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
    
   has_many :games, :class_name => "Game", :foreign_key => "creator_id", :dependent => :destroy 
   has_many :game_members,  :dependent => :destroy
-  has_many :comments
+  has_many :comments, :class_name => "Comment", :foreign_key => "from_user_id", :dependent => :destroy
   has_many :stats, :class_name => "Stat", :foreign_key => "winners_id",:dependent => :destroy 
   
   authenticates_with_sorcery!
