0b0c981739ef1e22ec314aad39456b9fd2d5c120
Daniel <gaeta.d@gmail.com>
2012-11-09 00:11:20 -0600

    change

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -415,11 +415,12 @@ def winners_and_losers
         wager = @search_results.wager
         stakes = @search_results.stakes
         private_or_not = @search_results.is_private
+        duration = @search_results.duration
         start_date = @search_results.game_start_date
         start_date =Date.new(start_date)
         start_date = start_date.strftime('%a %b %d')
         true_json =  { :status => "okay", :game_id => game_id, :creator_first_name => creator_first_name, :players => players, 
-        :wager => wager, :stakes => stakes, :is_private => private_or_not, :start_date => start_date}
+        :wager => wager, :stakes => stakes, :is_private => private_or_not, :duration => duration, :start_date => start_date}
         render(json: JSON.pretty_generate(true_json))
         else
         false_json = { :status => "fail."} 
