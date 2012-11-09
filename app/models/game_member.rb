492cfcbb80e012aa8c40d6477134b74612262280
Daniel <gaeta.d@gmail.com>
2012-11-09 00:30:30 -0600

    change

--- app/models/game_member.rb
+++ app/models/game_member.rb
@@ -1,6 +1,6 @@
 class GameMember < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
-  belongs_to :game, :dependent => :destroy
-  belongs_to :user, :dependent => :destroy
+  belongs_to :game 
+  belongs_to :user
   attr_accessible :checkins, :checkouts, :game_id, :successful_checks, :user_id
 end
