195369a0b6118f884404512f8c3b62159318e9f1
Daniel <gaeta.d@gmail.com>
2012-11-09 01:06:58 -0600

    change

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   
   belongs_to :user
   has_many :game_members, :dependent => :destroy
-  has_many :comments, :dependent => :destroy,  :through => :game_members
+  has_many :comments, :dependent => :destroy,  :through => :users
   attr_accessible :creator_id, :duration, :is_private, :wager, :players, :stakes, :game_start_date, :game_end_date, :creator_first_name
 end
