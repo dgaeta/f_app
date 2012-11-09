148b2201e46ffb0822c9bf9772f84e8f5cebc6fc
Daniel <gaeta.d@gmail.com>
2012-11-08 19:05:54 -0600

    change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -102,6 +102,7 @@
 
      respond_to do |format|
       if user
+        user = User.where(:email => params[:email]).first
         first_name = user.first_name
         last_name = user.last_name
         email = user.email
