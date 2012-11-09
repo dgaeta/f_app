587790e0e6809e8a239359c5ef70d8a4a5c1dfcb
Daniel <gaeta.d@gmail.com>
2012-11-09 01:04:05 -0600

    change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
- belongs_to :game_member
+ belongs_to :user
   attr_accessible :id, :message, :stamp, :from_user_id, :first_name, :last_name, :from_game_id
   #attr_accessor :game_member_id, :message, :stamp
  end
