b4dfd183f0b4191f0db64c8b2d8d4f1dfee90e50
Daniel <gaeta.d@gmail.com>
2012-11-12 01:07:17 -0600

    change

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -505,7 +505,8 @@ def winners_and_losers
         private_or_not = @search_results.is_private
         duration = @search_results.duration
         start_date = @search_results.game_start_date
-        start_date =Date.at(start_date)
+        start_date = Time.at(start_date)
+        start_date =Date.new(start_date)
         start_date = start_date.strftime('%a %b %d')
         true_json =  { :status => "okay", :game_id => game_id, :creator_first_name => creator_first_name, :players => players, 
         :wager => wager, :stakes => stakes, :is_private => private_or_not, :duration => duration, :start_date => start_date}
@@ -529,7 +530,8 @@ def winners_and_losers
         private_or_not = @search_results.is_private
         duration = @search_results.duration
         start_date = @search_results.game_start_date
-        start_date =Date.at(start_date)
+        start_date = Time.at(start_date)
+        start_date =Date.new(start_date)
         start_date = start_date.strftime("%-m/%-d/%-y")
         true_json =  { :status => "okay", :game_id => game_id, :creator_first_name => creator_first_name, :players => players, 
         :wager => wager, :stakes => stakes, :is_private => private_or_not, :duration => duration, :start_date => start_date}
