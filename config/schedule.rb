4501ca3c907cff529e7240b360168d7e856fa1db
Daniel <gaeta.d@gmail.com>
2012-11-13 04:04:44 -0600

    chnage

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every 1.day, :at => '12:01 am' do
+every 1.day, :at => '4:06 am' do
   runner "Game.auto_start_games"
 end
 
-every 1.day, :at => '2:50 am' do
+every 1.day, :at => '12:01 am' do
   runner "Game.auto_end_games"
 end
