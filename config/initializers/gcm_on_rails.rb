3f27af40beca161b17688e8ec40e66522b931ea3
Daniel <gaeta.d@gmail.com>
2012-12-29 13:15:40 -0600

    push

--- config/initializers/gcm_on_rails.rb
+++ config/initializers/gcm_on_rails.rb
@@ -1,4 +1,4 @@
 configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
-configatron.gcm_on_rails.api_key = '979881390942'
+configatron.gcm_on_rails.api_key = 'AIzaSyABFztuCfhqCsS_zLzmRv_q-dpDQ80K_gY'
 configatron.gcm_on_rails.app_name = 'com.example.fitsbypact'
 configatron.gcm_on_rails.delivery_format = 'json'
