aa7d47c6b6829339eae50e9d7f808f73cf6c127d
gaeta2 <gaeta.d@gmail.com>
2013-05-29 01:53:35 -0500

    js

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -5,7 +5,7 @@ class LandingsController < ApplicationController
     @landings = Landing.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html #index.html.erb
       format.json { render json: @landings }
     end
   end
