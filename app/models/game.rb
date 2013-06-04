feb5d5d52547f208d9e39e87aaebfd0da61ea9c7
gaeta2 <gaeta.d@gmail.com>
2013-06-03 20:58:33 -0400

    auto finish games fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -164,7 +164,7 @@ class Game < ActiveRecord::Base
     user = User.where(:id => user_id).first
     user.in_game = 0 
     user.save
-    stat = Stat.where(:winners_id => user.id)
+    stat = Stat.where(:winners_id => user.id).first
     stat.losses += 1 
     stat.games_played += 1 
     stat.save
