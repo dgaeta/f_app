9f7fdd328c004b73feaeae5ee791e68f74d501f6
gaeta2 <gaeta.d@gmail.com>
2013-06-16 20:06:07 -0400

    friendship scaffold

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,83 @@
+class FriendshipsController < ApplicationController
+  # GET /friendships
+  # GET /friendships.json
+  def index
+    @friendships = Friendship.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @friendships }
+    end
+  end
+
+  # GET /friendships/1
+  # GET /friendships/1.json
+  def show
+    @friendship = Friendship.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @friendship }
+    end
+  end
+
+  # GET /friendships/new
+  # GET /friendships/new.json
+  def new
+    @friendship = Friendship.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @friendship }
+    end
+  end
+
+  # GET /friendships/1/edit
+  def edit
+    @friendship = Friendship.find(params[:id])
+  end
+
+  # POST /friendships
+  # POST /friendships.json
+  def create
+    @user = User.where(:user_id => params[:user_id]).first
+    @friendship = @user.friendships.build(:friend_id => params[:friend_id])
+
+      if @friendship.save
+        true_json =  { :status => "okay"  }
+        render(json: JSON.pretty_generate(true_json))
+      else
+        false_json = { :status => "fail."} 
+        render(json: JSON.pretty_generate(false_json))
+      end
+    end
+  end
+
+  # PUT /friendships/1
+  # PUT /friendships/1.json
+  def update
+    @friendship = Friendship.find(params[:id])
+
+    respond_to do |format|
+      if @friendship.update_attributes(params[:friendship])
+        format.html { redirect_to @friendship, notice: 'Friendship was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @friendship.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /friendships/1
+  # DELETE /friendships/1.json
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+
+    respond_to do |format|
+      format.html { redirect_to friendships_url }
+      format.json { head :no_content }
+    end
+  end
+end
