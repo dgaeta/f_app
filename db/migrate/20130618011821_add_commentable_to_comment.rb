7edc5074d8a9d17685202f4a9a1a1d348984ab58
gaeta2 <gaeta.d@gmail.com>
2013-06-21 04:13:55 -0400

    adding filepicker

--- db/migrate/20130618011821_add_commentable_to_comment.rb
+++ db/migrate/20130618011821_add_commentable_to_comment.rb
@@ -1,8 +1,8 @@
 class AddCommentableToComment < ActiveRecord::Migration
   def up
-      
+       add_column :comments, :commentable_id, :integer, :commentable_type, :string
    end
   	
- add_column :comments, :commentable_id, :integer, :commentable_type, :string
+
   add_index :comments, [:commentable_id, :commentable_type]
 end
