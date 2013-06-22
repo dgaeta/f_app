3ff22279fa352a1c7ecef2048b728cacd40abfd3
gaeta2 <gaeta.d@gmail.com>
2013-06-21 23:12:54 -0400

    commentable migration

--- db/migrate/20130621195741_add_commentable_to_comments.rb
+++ db/migrate/20130621195741_add_commentable_to_comments.rb
@@ -1,10 +1,8 @@
 class AddCommentableToComments < ActiveRecord::Migration
   def change
   end
-  def up
-	   add_column :comments, :commentable_id, :integer
-	   add_column :comments, :commentable_type, :string
-   end
+    add_column :comments, :commentable_id, :integer
+	add_column :comments, :commentable_type, :string
   add_index :comments, [:commentable_id, :commentable_type]
 
 end
