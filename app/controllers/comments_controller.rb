ad382b0e314ec00443d5a9128254d79b060789c6
gaeta2 <gaeta.d@gmail.com>
2013-06-21 23:39:38 -0400

    private load commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -187,9 +187,9 @@ def game_comments
   end
 
 private
-  def load_commentable
-    resource, id = request.path.split('/')[1, 2]
-    @commentable = resource.singularize.classify.constantize.find(id)
-  end
+ def load_commentable
+  klass = [Game, User, ProfilePicture, Stat].detect { |c| params["#{c.name.underscore}_id"]}
+  @commentable = klass.find(params["#{klass.name.underscore}_id"])
+end
 
 end
