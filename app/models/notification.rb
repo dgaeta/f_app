8edbc380914371b7b7a42ecc6aca3a9771d327b7
gaeta2 <gaeta.d@gmail.com>
2013-06-22 02:31:50 -0400

    rename notifications to notication.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,4 @@
-class Notifications < ActiveRecord::Base
+class Notification < ActiveRecord::Base
   attr_accessible :content, :opened, :receiver_id, :sender_id
   belongs_to :notifiable, polymorphic: true
 end
