413bf7a19a85d893f12ce17060e27a30594606bf
gaeta2 <gaeta.d@gmail.com>
2013-07-15 15:40:35 -0400

    remove commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -208,10 +208,10 @@ def game_comments
     end
   end
 
-private
- def load_commentable
-  klass = [Game, User, ProfilePicture, Stat].detect { |c| params["#{c.name.underscore}_id"]}
-  @commentable = klass.find(params["#{klass.name.underscore}_id"])
-end
+#private
+ #def load_commentable
+  #klass = [Game, User, ProfilePicture, Stat].detect { |c| params["#{c.name.underscore}_id"]}
+  #@commentable = klass.find(params["#{klass.name.underscore}_id"])
+#end
 
 end
