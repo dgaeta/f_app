b9f62db194a7a043429a6b69ef1bfbb8b20e623f
gaeta2 <gaeta.d@gmail.com>
2013-07-23 00:25:58 -0500

    public games, and unless statement for cust id on create and join game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -373,9 +373,10 @@ def winners_and_losers
             :email => user_email ) 
             user.update_attributes(:customer_id => customer.id)
 
-            if user.customer == 0 
+            if user.customer_id == 0 
               bad_cc_json = { :status => "invalid credit card"} 
               render(json: JSON.pretty_generate(bad_cc_json))
+              return
             end 
 
             # Now, make a stripe column for database table 'users'
