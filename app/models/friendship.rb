bb5892d03f5628dff6e580bbac3f97778c12ba5e
gaeta2 <gaeta.d@gmail.com>
2013-07-29 23:50:18 -0400

    friends model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ActiveRecord::Base
-  attr_accessible :friend_id, :user_id, :status
+  attr_accessible :friend_id, :user_id, :status, :friend_first_name, :friend_last_name
   belongs_to :user
   belongs_to :friend, :class_name => "User"
   has_many :notifications, as: :notifiable
