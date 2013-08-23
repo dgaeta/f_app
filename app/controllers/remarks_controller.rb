0ba7f320ec9da046dc1b3df68dfb947c9d428bcb
gaeta2 <gaeta.d@gmail.com>
2013-08-23 00:40:58 -0400

    users deliver profile picture

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -39,7 +39,7 @@ class RemarksController < ApplicationController
 	  	:message => remark.message, 
 	  	:from_user_id => remark.from_user_id,
 	  	:commentable_id => remark.remarkable_id,
-	  	:profile_picture_status_hash => User.deliver_profile_picture }
+	  	:profile_picture_status_hash => User.deliver_profile_picture(from_user_id) }
 
 	  	if comment 
 		  	success_json = { :status => "okay", :remarks => remarks} 
